55afc898913196cada760cdaf880aceb0b985356
Michael P <102651473+michmich16@users.noreply.github.com>
2024-09-03 09:32:40 +0200

    ldnwj

---
+++ routing-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.scss'
+
+function App() {
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
